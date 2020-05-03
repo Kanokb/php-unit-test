32893de3aecbfe957638904e31c555b096391833
kanok bunluepuet <kanok.bun@ascendcorp.com>
2020-05-03 21:09:07 +0700

    function culculator

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
 
-echo "Mhaimii ja ja";
+$calculator = new Calculator();
+$result = $calculator->sum(400, 400);
+
+echo $result;
+
+class Calculator{
+    function sum($num1, $num2){
+        return $num1 + $num2;
+    }
+}
