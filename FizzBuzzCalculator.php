c2c2be2a1d0c521c151c7390838e60462e200752
Chaiwat Matarak <chaiwat.mat@gmail.com>
2020-05-06 13:09:53 +0700

    add FizzBuzzCalculator

---
+++ FizzBuzzCalculator.php
@@ -0,0 +1,27 @@
+<?php
+
+class FizzBuzzCalculator{
+    function calculate($number){
+        if ($i % 35 === 0) {
+            return "FizzBuzzWuzz";
+        }
+        else if ($i % 21 === 0) {
+            return "FizzWuzz";
+        }
+        else if ($i % 15 === 0) {
+            return "FizzBuzz";
+        }
+        else if($i % 7 == 0){
+            return "Wuzz";
+        }
+        else if($i % 5 == 0){
+            return "Buzz";
+        }
+        else if($i % 3 == 0){
+            return "Fizz";
+        }
+        else {
+            return "$i";
+        }
+    }
+}
